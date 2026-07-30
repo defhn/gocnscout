6df48efacfc192b58a89e52a7f5721c67b2e5397
defhn <defhn@users.noreply.github.com>
2026-07-30 16:48:11 +0800

    feat(blog): compress cover images to webp, upload to Cloudflare R2, and update DB cover URLs

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   output: "standalone",
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*.r2.dev",
+      },
+      {
+        protocol: "https",
+        hostname: "pub-f27e22c13e5e4342a67089e6fa5ed831.r2.dev",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
